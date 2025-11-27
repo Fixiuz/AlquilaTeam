8f5740402987279b24f46dd9b1b5d4b7db2a1e1b
Julio Fidanza <JulioFidanza@gmail.com>
2025-11-27 15:53:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4799479286-2493d",
+  "appId": "1:582353989118:web:815643a9fbf1dedf7e64af",
+  "apiKey": "AIzaSyDNklxn4IakD52-J2re78p8yrvsqr8dgVw",
+  "authDomain": "studio-4799479286-2493d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "582353989118"
+};
